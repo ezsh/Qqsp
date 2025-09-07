ezsh/Qqsp

commit 3d7565c2162f9eee1d5c4c2b0f3a5a6e55d2e0f9

Adapt to upstream QSP changes

callbacks_gui.cpp
@@ -124,6 +124,8 @@ void QSPCallBacks::SetTimer(int msecs)
 
 void QSPCallBacks::RefreshInt(QSP_BOOL isForced, QSP_BOOL isNewDesc)
 {
+	using namespace Qt::StringLiterals;
+
 	QSPVariant val;
 	bool isScroll, isCanSave;
 	if (m_frame->IsQuit()) {
@@ -140,7 +142,8 @@ void QSPCallBacks::RefreshInt(QSP_BOOL isForced, QSP_BOOL isNewDesc)
 	m_isHtml = QSPGetVarValue(L"USEHTML"_qsp, 0, &val) && QSP_NUM(val);
 	// -------------------------------
 	m_frame->GetVars()->SetIsHtml(m_isHtml);
-	if (QSPIsVarsDescChanged()) {
+	const int windowStateChanges = QSPGetWindowsChangedState();
+	if (windowStateChanges & QSP_WIN_VARS) {
 		m_frame->EnableControls(false, true);
 		if (m_isAllowHTML5Extras) {
 			if (QSPGetVarValue(L"SETSTATHEAD"_qsp, 0, &val) && QSP_STR(val).Str) {
@@ -155,7 +158,7 @@ void QSPCallBacks::RefreshInt(QSP_BOOL isForced, QSP_BOOL isNewDesc)
 	// -------------------------------
 	isScroll = !isNewDesc;
 	m_frame->GetDesc()->SetIsHtml(m_isHtml);
-	if (QSPIsMainDescChanged()) {
+	if (windowStateChanges & QSP_WIN_MAIN) {
 		m_frame->EnableControls(false, true);
 		if (m_isAllowHTML5Extras) {
 			if (QSPGetVarValue(L"SETMAINDESCHEAD"_qsp, 0, &val) && QSP_STR(val).Str) {
@@ -170,7 +173,7 @@ void QSPCallBacks::RefreshInt(QSP_BOOL isForced, QSP_BOOL isNewDesc)
 	// -------------------------------
 	m_frame->GetActions()->SetIsHtml(m_isHtml);
 	m_frame->GetActions()->SetIsShowNums(m_frame->IsShowHotkeys());
-	if (QSPIsActionsChanged()) {
+	if (windowStateChanges & QSP_WIN_ACTS) {
 		std::vector<QSPListItem> actions = QSPTools::qspActions();
 		m_frame->GetActions()->BeginItems();
 		for (const QSPListItem& li: actions) {
@@ -180,11 +183,13 @@ void QSPCallBacks::RefreshInt(QSP_BOOL isForced, QSP_BOOL isNewDesc)
 	}
 	m_frame->GetActions()->SetSelection(QSPGetSelActionIndex());
 	m_frame->GetObjects()->SetIsHtml(m_isHtml);
-	if (QSPIsObjectsChanged()) {
-		std::vector<QSPListItem> objects = QSPTools::qspObjects();
+	if (windowStateChanges & QSP_WIN_OBJS) {
+		std::vector<QSPObjectItem> objects = QSPTools::qspObjects();
 		m_frame->GetObjects()->BeginItems();
-		for (const QSPListItem& li: objects) {
-			m_frame->GetObjects()->AddItem(QSPTools::GetCaseInsensitiveFilePath(m_gamePath, QSPTools::qspStrToQt(li.Image)), QSPTools::qspStrToQt(li.Name));
+		for (const QSPObjectItem& li: objects) {
+			m_frame->GetObjects()->AddItem(
+				QSPTools::GetCaseInsensitiveFilePath(m_gamePath, QSPTools::qspStrToQt(li.Image)),
+				"%1 [%2]"_L1.arg(QSPTools::qspStrToQt(li.Name), QSPTools::qspStrToQt(li.Title)));
 		}
 		m_frame->GetObjects()->EndItems();
 	}

comtools.cpp
@@ -218,12 +218,11 @@ QColor QSPTools::wxtoQColor(int wxColor)
 }
 
 namespace {
-	using qspObjectGetter = int(QSPListItem* items, int bufferSize);
-
-	std::vector<QSPListItem> listQspItems(qspObjectGetter getter)
+	template <typename Item, typename Getter = int(Item* items, int bufferSize)>
+	std::vector<Item> listQspItems(Getter getter)
 	{
 		int count = getter(nullptr, 0);
-		std::vector<QSPListItem> res(count);
+		std::vector<Item> res(count);
 		getter(res.data(), res.size());
 		return res;
 	}
@@ -231,12 +230,12 @@ namespace {
 
 std::vector<QSPListItem> QSPTools::qspActions()
 {
-	return listQspItems(QSPGetActions);
+	return listQspItems<QSPListItem>(QSPGetActions);
 }
 
-std::vector<QSPListItem> QSPTools::qspObjects()
+std::vector<QSPObjectItem> QSPTools::qspObjects()
 {
-	return listQspItems(QSPGetObjects);
+	return listQspItems<QSPObjectItem>(QSPGetObjects);
 }
 
 bool QSPTools::loadGameFile(QString path)

comtools.h
@@ -22,7 +22,7 @@ public:
 	static QColor wxtoQColor(int wxColor);
 
 	static std::vector<QSPListItem> qspActions();
-	static std::vector<QSPListItem> qspObjects();
+	static std::vector<QSPObjectItem> qspObjects();
 
 	static bool loadGameFile(QString path);
 	static bool reloadGame(const QString& gameFilePath);

commit 57a73094918e4f1ff9ecb15c1018569c938682f9

Fix handling window type in QSPCallBacks::ShowPane()
The type parameter is now a window type flags.

Fixes: 3d7565c2162f ("Adapt to upstream QSP changes")

callbacks_gui.cpp
@@ -269,11 +269,20 @@ void QSPCallBacks::ShowPane(int type, QSP_BOOL isShow)
 	if (m_frame->IsQuit()) {
 		return;
 	}
-	switch (type) {
-		case QSP_WIN_ACTS: m_frame->GetActionsDock()->setVisible(isShow != QSP_FALSE); break;
-		case QSP_WIN_OBJS: m_frame->GetObjectsDock()->setVisible(isShow != QSP_FALSE); break;
-		case QSP_WIN_VARS: m_frame->GetVarsDock()->setVisible(isShow != QSP_FALSE); break;
-		case QSP_WIN_INPUT: m_frame->GetInputDock()->setVisible(isShow != QSP_FALSE); break;
+	if (type & QSP_WIN_ACTS) {
+		m_frame->GetActionsDock()->setVisible(isShow != QSP_FALSE);
+	}
+	if (type & QSP_WIN_OBJS) {
+		m_frame->GetObjectsDock()->setVisible(isShow != QSP_FALSE);
+	}
+	if (type & QSP_WIN_VARS) {
+		m_frame->GetVarsDock()->setVisible(isShow != QSP_FALSE);
+	}
+	if (type & QSP_WIN_INPUT) {
+		m_frame->GetInputDock()->setVisible(isShow != QSP_FALSE);
+	}
+	if (type & QSP_WIN_VIEW) {
+		m_frame->GetImageDock()->setVisible(isShow != QSP_FALSE);
 	}
 }
 